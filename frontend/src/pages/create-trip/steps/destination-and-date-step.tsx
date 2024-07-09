a815067d17d6dd689aa51aeb15b4bcb8b791aa03
MarquinhoCF <marcos13@gmail.com>
2024-07-09 20:13:41 -0300

    feat: Visual da página Trip Details

--- frontend/src/pages/create-trip/steps/destination-and-date-step.tsx
+++ frontend/src/pages/create-trip/steps/destination-and-date-step.tsx
@@ -27,13 +27,13 @@ export function DestinationAndDateStep({
 
             { isGuestsInputOpen ? (
                 <button onClick={closeGuestsInput} className='bg-zinc-800 text-zinc-200 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-zinc-700'>
-                Alterar local/data
-                <Settings2 className="size-5" />
+                    Alterar local/data
+                    <Settings2 className="size-5" />
                 </button>
             ) : (
                 <button onClick={openGuestsInput} className='bg-lime-300 text-lime-950 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-lime-400' >
-                Continuar
-                <ArrowRight className="size-5" />
+                    Continuar
+                    <ArrowRight className="size-5" />
                 </button>
             )}
         </div>
