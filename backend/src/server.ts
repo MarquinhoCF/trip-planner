e6c71f996a26943b0fa3efe37a0156848782f408
MarquinhoCF <marcos13@gmail.com>
2024-07-11 15:54:03 -0300

    feat: Rota para listar os participantes

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,12 +2,13 @@ import fastify from "fastify";
 import cors from "@fastify/cors";
 import { createTrip } from "./routes/create-trip";
 import { confirmTrip } from "./routes/confirm-trip";
+import { confirmParticipant } from "./routes/confirm-participant";
 import { createActivity } from "./routes/create-activity";
 import { getActivities } from "./routes/get-activities";
 import { createLink } from "./routes/create-link";
 import { getLinks } from "./routes/get-links";
+import { getParticipants } from "./routes/get-participants";
 import { validatorCompiler, serializerCompiler } from "fastify-type-provider-zod";
-import { confirmParticipant } from "./routes/confirm-participant";
 
 const app = fastify();
 
@@ -25,6 +26,7 @@ app.register(createActivity);
 app.register(getActivities);
 app.register(createLink);
 app.register(getLinks);
+app.register(getParticipants);
 
 app.listen({port: 3333}).then(() => {
     console.log("Server is running on port 3333");
