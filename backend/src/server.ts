8290acac00f694d374ac19017fdfab3eb1c24bda
MarquinhoCF <marcos13@gmail.com>
2024-07-11 16:31:21 -0300

    feat: Rota para listar os detalhes do participante

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -11,6 +11,7 @@ import { getParticipants } from "./routes/get-participants";
 import { createInvite } from "./routes/create-invite";
 import { updateTrip } from "./routes/update-trip";
 import { getTripDetails } from "./routes/get-trip-details";
+import { getParticipantDetails } from "./routes/get-participant-details";
 import { validatorCompiler, serializerCompiler } from "fastify-type-provider-zod";
 
 const app = fastify();
@@ -33,6 +34,7 @@ app.register(getParticipants);
 app.register(createInvite);
 app.register(updateTrip);
 app.register(getTripDetails);
+app.register(getParticipantDetails);
 
 app.listen({port: 3333}).then(() => {
     console.log("Server is running on port 3333");
