db1bb17c8f4219f2f629b5ec3b786f5bfdbca703
MarquinhoCF <marcos13@gmail.com>
2024-07-11 15:45:35 -0300

    feat: Criando as rotas de criar e listar atividades e links

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,6 +2,10 @@ import fastify from "fastify";
 import cors from "@fastify/cors";
 import { createTrip } from "./routes/create-trip";
 import { confirmTrip } from "./routes/confirm-trip";
+import { createActivity } from "./routes/create-activity";
+import { getActivities } from "./routes/get-activities";
+import { createLink } from "./routes/create-link";
+import { getLinks } from "./routes/get-links";
 import { validatorCompiler, serializerCompiler } from "fastify-type-provider-zod";
 import { confirmParticipant } from "./routes/confirm-participant";
 
@@ -17,6 +21,10 @@ app.register(cors, {
 app.register(createTrip);
 app.register(confirmTrip);
 app.register(confirmParticipant);
+app.register(createActivity);
+app.register(getActivities);
+app.register(createLink);
+app.register(getLinks);
 
 app.listen({port: 3333}).then(() => {
     console.log("Server is running on port 3333");
