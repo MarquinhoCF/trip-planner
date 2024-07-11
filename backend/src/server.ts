c8dcc4d36f10230f23cb71642d74ae479af8ddab
MarquinhoCF <marcos13@gmail.com>
2024-07-11 00:43:34 -0300

    feat: Rota de confirmação do participante finalizada

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,6 +3,7 @@ import cors from "@fastify/cors";
 import { createTrip } from "./routes/create-trip";
 import { confirmTrip } from "./routes/confirm-trip";
 import { validatorCompiler, serializerCompiler } from "fastify-type-provider-zod";
+import { confirmParticipant } from "./routes/confirm-participant";
 
 const app = fastify();
 
@@ -15,6 +16,7 @@ app.register(cors, {
 
 app.register(createTrip);
 app.register(confirmTrip);
+app.register(confirmParticipant);
 
 app.listen({port: 3333}).then(() => {
     console.log("Server is running on port 3333");
