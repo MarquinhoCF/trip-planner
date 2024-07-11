8ac9941cff8d242cd2e65dd20f05874727e4b2fb
MarquinhoCF <marcos13@gmail.com>
2024-07-11 16:13:54 -0300

    feat: Criando rota de atualização da viagem

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -9,7 +9,9 @@ import { createLink } from "./routes/create-link";
 import { getLinks } from "./routes/get-links";
 import { getParticipants } from "./routes/get-participants";
 import { createInvite } from "./routes/create-invite";
+import { updateTrip } from "./routes/update-trip";
 import { validatorCompiler, serializerCompiler } from "fastify-type-provider-zod";
+
 
 const app = fastify();
 
@@ -29,6 +31,7 @@ app.register(createLink);
 app.register(getLinks);
 app.register(getParticipants);
 app.register(createInvite);
+app.register(updateTrip);
 
 app.listen({port: 3333}).then(() => {
     console.log("Server is running on port 3333");
