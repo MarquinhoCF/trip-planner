9d6e2f4ff0487216ba74e938a078868adb42bf34
MarquinhoCF <marcos13@gmail.com>
2024-07-12 01:20:50 -0300

    feat: Exibir os links importantes e cadastrar novo link

--- frontend/src/pages/trip-details/guests.tsx
+++ frontend/src/pages/trip-details/guests.tsx
@@ -19,35 +19,45 @@ export function Guests() {
         api.get(`/trips/${tripId}/participants`).then(response => setParticipants(response.data.participants));
     }, [tripId]);
 
+    const length = participants ? participants.length : 0;
+
     return (
         <div className="space-y-6">
             <h2 className="font-semibold text-xl">Convidados</h2>
             
             <div className="space-y-5">
-                { participants?.map((participant, index) => {
-                    return (
-                        <div key={participant.id} className="flex items-center justify-between gap-4">
-                            <div className="space-y-1.5 flex-1">
-                                <span className="block font-medium text-zinc-100">{ participant.name ?? `Convidado ${index}`}</span>
-                                <span className="block text-sm text-zinc-400 truncate">
-                                    { participant.email }
-                                </span>
+                { length > 0 ? (
+                    participants?.map((participant, index) => {
+                        return (
+                            <div key={participant.id} className="flex items-center justify-between gap-4">
+                                <div className="space-y-1.5 flex-1">
+                                    <span className="block font-medium text-zinc-100">{ participant.name ?? `Convidado ${index}`}</span>
+                                    <span className="block text-sm text-zinc-400 truncate">
+                                        { participant.email }
+                                    </span>
+                                </div>
+    
+                                { participant.is_confirmed ? (
+                                    <CheckCircle2 className="size-5 shrink-0 text-green-400 " />
+                                ) : (
+                                    <CircleDashed className="text-zinc-400" />
+                                )}
                             </div>
-
-                            { participant.is_confirmed ? (
-                                <CheckCircle2 className="size-5 shrink-0 text-green-400 " />
-                            ) : (
-                                <CircleDashed className="text-zinc-400" />
-                            )}
-                        </div>
-                    );
-                })}
+                        );
+                    })
+                ) : (
+                    <p className="text-zinc-500 text-sm">
+                        Nenhum convidado cadastrado nessa viagem.
+                    </p>
+                )}
             </div>
 
             <Button variant="secundary" size="full">
                 Gerenciar convidados
                 <UserCog className="size-5" />
             </Button>
+
+            
         </div>
     );
 }
--- frontend/src/pages/trip-details/important-links.tsx
+++ frontend/src/pages/trip-details/important-links.tsx
@@ -1,39 +1,71 @@
 import { Link2, Plus } from "lucide-react";
 import { Button } from "../../components/button";
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { api } from "../../lib/axios";
+import { CreateLinkModal } from "./create-link-modal";
+
+interface Link {
+    id: string;
+    title: string;
+    url: string;
+}
 
 export function ImportantLinks() {
+    const [ isOpenCreateLinkModal, setIsOpenCreateLinkModal ] = useState(false);
+
+    const { tripId } = useParams();
+    const [ links, setLinks ] = useState<Link[] | undefined>([]);
+
+    useEffect(() => {
+        api.get(`/trips/${tripId}/links`).then(response => setLinks(response.data.links));
+    }, [tripId]);
+
+    const length = links ? links.length : 0;
+
+    function openCreateLinkModela() {
+        setIsOpenCreateLinkModal(true);
+    }
+
+    function closeCreateLinkModal() {
+        setIsOpenCreateLinkModal(false);
+    }
+
     return (
         <div className="space-y-6">
             <h2 className="font-semibold text-xl">Links Importantes</h2>
             
             <div className="space-y-5">
-                <div className="flex items-center justify-between gap-4">
-                    <div className="space-y-1.5 flex-1">
-                        <span className="block font-medium text-zinc-100">Reserva do Airbnb</span>
-                        <a href="#" className="block text-xs text-zinc-400 truncate hover:text-zinc-200">
-                            https://www.airbnb.com.br/rooms/104700011
-                        </a>
-                    </div>
+                { length > 0 ? (
+                    links?.map(link => (
+                        <div key={link.id} className="flex items-center justify-between gap-4">
+                            <div className="space-y-1.5 flex-1">
+                                <span className="block font-medium text-zinc-100">{link.title}</span>
+                                <a href={link.url} className="block text-xs text-zinc-400 truncate hover:text-zinc-200">
+                                    {link.url}
+                                </a>
+                            </div>
 
-                    <Link2 className="text-zinc-400 " />
-                </div>
+                            <Link2 className="text-zinc-400" />
+                        </div>
+                    ))
+                ) : (
+                    <p className="text-zinc-500 text-sm">
+                        Nenhum link cadastrado nessa viagem.
+                    </p>
+                )}
+            </div> 
 
-                <div className="flex items-center justify-between gap-4">
-                    <div className="space-y-1.5 flex-1">
-                        <span className="block font-medium text-zinc-100">Reserva do Airbnb</span>
-                        <a href="#" className="block text-xs text-zinc-400 truncate hover:text-zinc-200">
-                            https://www.airbnb.com.br/rooms/104700011
-                        </a>
-                    </div>
-
-                    <Link2 className="text-zinc-400 " />
-                </div>
-            </div>
-
-            <Button variant="secundary" size="full">
+            <Button onClick={openCreateLinkModela} variant="secundary" size="full">
                 Cadastrar novo link
                 <Plus className="size-5" />
             </Button>
+
+            { isOpenCreateLinkModal && (
+                <CreateLinkModal
+                    closeCreateLinkModal={closeCreateLinkModal}
+                />
+            )}
         </div>
     );
 }
